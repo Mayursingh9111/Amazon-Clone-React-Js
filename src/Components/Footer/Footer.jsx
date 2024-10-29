1c7c8d7a9808928f7adc6fd82c0a7b306b439e0b
Mayursingh9111 <mayursingh9112001@gmail.com>
2024-10-29 23:25:33 +0530

    Header and Footer component created

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import Footertop from "./Footertop";
+import FooterMiddle from "./FooterMiddle";
+import FooterEnd from "./FooterEnd";
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <Footertop />
+        <FooterMiddle />
+        <FooterEnd />
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
