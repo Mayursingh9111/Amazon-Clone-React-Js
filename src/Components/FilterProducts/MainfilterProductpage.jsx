e89a1abee14b358ed0e4a6abbc995036af8a9888
Mayursingh9111 <mayursingh9112001@gmail.com>
2024-10-30 21:16:24 +0530

    Filter componenet

---
+++ src/Components/FilterProducts/MainfilterProductpage.jsx
@@ -0,0 +1,16 @@
+import SortedProduct from "./SortedProduct";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+const FilterProducts = () => {
+  return (
+    <>
+      <section name="FilterProducts">
+        <Header />
+        <SortedProduct />
+        <Footer />
+      </section>
+    </>
+  );
+};
+
+export default FilterProducts;
